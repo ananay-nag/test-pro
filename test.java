4c83c5badd0483fdd0a51829f4ebf9dae929daf0
go-ananay-nag <go-ananay-nag@gitlab.com>
2021-06-05 18:15:17 +0530

    all set

---
+++ test.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class test{
+    static {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        if(s>=10){
+            System.out.println("hello1");
+            System.exit(1);
+        }
+         
+    }
+    public static void  main(String ananay []){
+        {
+            System.out.println("hello 3 ");
+        }
+        System.out.println("hello");
+    }
+    static {
+         System.out.println("hello 2 ");
+    }
+}
